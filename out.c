f93eba8e400b46ada648ba186eda0929f9de91c3
RusyaKhalmatov <rusya.khalmatov@gmail.com>
2017-11-30 11:54:52 +0500

    Out file has been changed

--- out.c
+++ out.c
@@ -3,13 +3,43 @@
 int main()
 {
 
-char input[100];
+  /*char input[100];
   printf("WElcome enter some string here: ");
   //scanf("%s", input);
   fgets(input, sizeof(input), stdin);
   printf("You entered:");
   puts(input);
-  //printf("%s\n", input);
+  //printf("%s\n", input);*/
+
+  int choice;
+
+  printf("Enter your choice: ");
+  scanf("%d",&choice);
+  printf("You entered: %d\n",choice);
+  int ans=1;
+  while (ans!=0) {
+  /* code */
+  switch(choice)
+  {
+    case 1:
+      printf("ONe");
+      printf("Choice: ");
+      scanf("%d", &ans);
+      break;
+    case 2:
+      printf("TWo");
+      printf("Choice: ");
+      scanf("%d", &ans);
+      break;
+    case 3:
+      printf("Three");
+      printf("Choice: ");
+      scanf("%d", &ans);
+      break;
+
+  }
+}
+
 
 
   return 0;
